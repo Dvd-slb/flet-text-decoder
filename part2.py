9737f45a98ffbb243a2c6ace33094ca6e8d63f28
David Slaboň <slabon.david94@gmail.com>
2023-02-19 23:02:51 +0100

    kofinálnísmetické úpravy

--- part2.py
+++ part2.py
@@ -3,6 +3,7 @@ from time import sleep
 from cipher import cipher_encode, cipher_decode
 import pyperclip
 
+
 # Vytvoření okna
 def main(page: flet.Page):
     page.window_height = 600
@@ -34,24 +35,25 @@ def main(page: flet.Page):
     def encode_choice(e):
         shift_number.on_submit = encode
         main_container.content.clean()
-        main_container.content = flet.Column([flet.Container(flet.Row([text_to_encode], alignment=flet.MainAxisAlignment.CENTER)),
-                                              flet.Row([flet.Column(
-                                                  [(flet.Container(back_button, padding=flet.padding.only(left=10)))]),
+        main_container.content = flet.Column(
+            [flet.Container(flet.Row([text_to_encode], alignment=flet.MainAxisAlignment.CENTER)),
+             flet.Row([flet.Column(
+                 [(flet.Container(back_button, padding=flet.padding.only(left=10)))]),
 
-                                                  flet.Container(content=flet.Row([
-                                                      flet.Column([flet.Container(
-                                                          flet.Text("Kód:", size=18, weight=flet.FontWeight.BOLD),
-                                                          padding=flet.padding.only(left=4, right=-8))]),
-                                                      flet.Column([flet.Container(shift_number, bgcolor="#28282b",
-                                                                                  border_radius=10)])]),
-                                                      padding=flet.padding.only(left=5)),
+                 flet.Container(content=flet.Row([
+                     flet.Column([flet.Container(
+                         flet.Text("Kód:", size=18, weight=flet.FontWeight.BOLD),
+                         padding=flet.padding.only(left=4, right=-8))]),
+                     flet.Column([flet.Container(shift_number, bgcolor="#28282b",
+                                                 border_radius=10)])]),
+                     padding=flet.padding.only(left=5)),
 
-                                                  flet.Column([flet.Container(encode_button,
-                                                                              padding=flet.padding.only(
-                                                                                  left=9))],
-                                                              alignment=flet.MainAxisAlignment.CENTER)]),
-                                              flet.Row([encoded_text], alignment=flet.MainAxisAlignment.CENTER)],
-                                             alignment=flet.MainAxisAlignment.CENTER, spacing=7)
+                 flet.Column([flet.Container(encode_button,
+                                             padding=flet.padding.only(
+                                                 left=9))],
+                             alignment=flet.MainAxisAlignment.CENTER)]),
+             flet.Row([encoded_text], alignment=flet.MainAxisAlignment.CENTER)],
+            alignment=flet.MainAxisAlignment.CENTER, spacing=7)
         shift_number.label = "0-9999"
         page.update()
 
@@ -192,8 +194,6 @@ def main(page: flet.Page):
     def copy_encoded_text(e):
         pyperclip.copy(encoded_field.value)
         data = encoded_field.value
-        # subprocess.run("pbcopy", text=True, input=data)
-
 
     # Dát na výběr, jestli zprávu zakódovat nebo odkódovat
     en_button = flet.ElevatedButton(content=flet.Text(" Chci zprávu\nZAKÓDOVAT", size=25, font_family="Lucida Sans"),
@@ -220,13 +220,14 @@ def main(page: flet.Page):
     page.add(main_container)
 
     # Pokud zakódovat, tak se okno přemění:
-    # Vytvořit pole, do kterého se bude vkládat text k zakódování
+
     # Tlačítko na vymazání zadaného textu k zakódování "#22292f"
     erase_encode_button = flet.Container(
         flet.IconButton(icon=flet.icons.DELETE_FOREVER_OUTLINED, icon_color="white", icon_size=25),
         height=200, on_click=lambda e: erase_encode_text(e),
         border_radius=flet.border_radius.only(topLeft=20, bottomLeft=20, topRight=10, bottomRight=10),
         bgcolor="#22292f")
+
     # Vytvořit pole, do kterého se bude vkládat text k zakódování
     field_to_encode = flet.TextField(label="Zpráva k zakódování", width=252, height=200,
                                      color="#a6a6ac", autofocus=True, cursor_color="#a6a6ac",
@@ -235,14 +236,16 @@ def main(page: flet.Page):
                                      border_color="transparent", text_align=flet.TextAlign.JUSTIFY,
                                      label_style=flet.TextStyle(size=20, color="#d2d2d5",
                                                                 weight=flet.FontWeight.BOLD))
-    text_to_encode = flet.Container(border=flet.border.all(2, "black"), bgcolor="#28282b", border_radius=10, width=300, padding=flet.padding.only(right=3),
+
+    text_to_encode = flet.Container(border=flet.border.all(2, "black"), bgcolor="#28282b", border_radius=10, width=300,
+                                    padding=flet.padding.only(right=3),
                                     content=flet.Column(spacing=0,
                                                         controls=[
                                                             flet.Row(alignment=flet.MainAxisAlignment.END, spacing=0,
                                                                      controls=[
                                                                          field_to_encode,
                                                                          erase_encode_button])
-                                                            ]
+                                                        ]
                                                         ),
                                     )
 
@@ -251,6 +254,7 @@ def main(page: flet.Page):
                                   border_radius=10, border_color="black", text_align=flet.TextAlign.CENTER,
                                   tooltip="Kód pro následné odkódování\nMaximálně 4 znaky\nPouze číslice",
                                   on_focus=erase_shift_numbers, label_style=flet.TextStyle(color="#d2d2d5", size=14))
+
     # Tlačítko, které zprávu zakóduje
     encode_button = flet.ElevatedButton(text="Zakódovat", icon=flet.icons.MOVE_DOWN, color="#a6a6ac",
                                         bgcolor="#28282b", on_click=encode,
@@ -258,12 +262,14 @@ def main(page: flet.Page):
                                                                shape=flet.RoundedRectangleBorder(radius=15),
                                                                side=flet.border.BorderSide(width=1,
                                                                                            color="black")))
+
     # Tlačítko na zkopírování textu
     copy_encode_button = flet.Container(flet.IconButton(icon=flet.icons.COPY, icon_size=25, icon_color="white"),
                                         height=200, on_click=lambda e: copy_encoded_text(e),
                                         border_radius=flet.border_radius.only(topLeft=20, bottomLeft=20, topRight=10,
                                                                               bottomRight=10),
                                         bgcolor="#22292f")
+
     # Vypsání zakódovaného textu
     encoded_field = flet.TextField(label="Zakódovaná zpráva", value=" ", width=255, height=200,
                                    color="#a6a6ac", cursor_color="#a6a6ac", read_only=True, border_color="transparent",
@@ -271,6 +277,7 @@ def main(page: flet.Page):
                                    multiline=True, min_lines=6, border_radius=10,
                                    label_style=flet.TextStyle(size=20, color="#d2d2d5",
                                                               weight=flet.FontWeight.BOLD))
+
     encoded_text = flet.Container(bgcolor="#28282b", border_radius=10, width=300, border=flet.border.all(2, "black"),
                                   content=flet.Column(
                                       controls=[
@@ -297,10 +304,6 @@ def main(page: flet.Page):
         height=200, on_click=lambda e: erase_decode_text(e), padding=flet.padding.only(right=-1),
         border_radius=flet.border_radius.only(topLeft=20, bottomLeft=20, topRight=10, bottomRight=10),
         bgcolor="#22292f")
-    # gradient=flet.LinearGradient(["#28282b", "transparent"],
-    #   begin=flet.alignment.center_left, end=flet.alignment.center_right))
-    # border_radius=25, border=flet.border.all(width=0.2, color="#3d3d48"))
-    # border=flet.border.only(left=flet.border.BorderSide(width=0.2, color="silver")))
 
     # Vytvořit pole, do kterého se bude vkládat text k odkódování
     field_to_decode = flet.TextField(label="Zpráva k odkódování", width=255, height=200,
@@ -310,6 +313,7 @@ def main(page: flet.Page):
                                      border_color="transparent",
                                      label_style=flet.TextStyle(size=20, color="#d2d2d5",
                                                                 weight=flet.FontWeight.BOLD))
+
     text_to_decode = flet.Container(border=flet.border.all(2, "black"), bgcolor="#28282b", border_radius=10, width=300,
                                     content=flet.Column(spacing=1,
                                                         controls=[
@@ -317,10 +321,10 @@ def main(page: flet.Page):
                                                                      controls=[
                                                                          field_to_decode,
                                                                          erase_decode_button])
-                                                            ]
+                                                        ]
                                                         ),
                                     )
-    # Tlačítko, které zprávu odkóduje (posun o -43)
+
     # Vypsání odkódovaného textu
     decoded_field = flet.TextField(label="Odkódovaná zpráva", value=" ", width=300, height=200,
                                    color="#a6a6ac", cursor_color="#a6a6ac", read_only=True,
@@ -328,9 +332,10 @@ def main(page: flet.Page):
                                    multiline=True, min_lines=6, border_radius=10, border_color="black",
                                    label_style=flet.TextStyle(size=20, color="#d2d2d5",
                                                               weight=flet.FontWeight.BOLD))
+
     decoded_text = flet.Container(decoded_field, bgcolor="#28282b", border_radius=10)
 
-    # Tlačítko "zpět na výběr code/decode" (ideálně šipka)
+    # Tlačítko "zpět na výběr code/decode"
     decode_button = flet.ElevatedButton(text="Odkódovat", icon=flet.icons.MOVE_DOWN, color="#a6a6ac",
                                         bgcolor="#28282b", on_click=decode,
                                         style=flet.ButtonStyle(overlay_color="#303034",
@@ -340,6 +345,5 @@ def main(page: flet.Page):
 
 
 # Spuštění programu
-
 if __name__ == "__main__":
     flet.app(target=main)
